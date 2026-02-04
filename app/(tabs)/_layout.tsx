5cb80f695cd0aa3508d0621b5822dc52230b5cc2
Nina Wiik <ninawiik1@outlook.com>
2026-02-04 14:42:29 +0100

    Added AuthGuard

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ color, size }) => <AntDesign name="home" size={size} color={color} /> }} />
+      <Tabs.Screen name="login" options={{ title: "Login" }} />
+    </Tabs>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,34 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  const isAuth = false;
+
+  useEffect(() => {
+    // delay was set to get over the error, error: Attempted to navigate before mounting the Root Layout component
+     const timerId = setTimeout(() => {
+      if (!isAuth) {
+        router.replace("/auth");
+      }
+    }, 500); // 500 milliseconds = 2 second delay
+
+    // Cleanup function to clear the timeout
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, []);
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <RouteGuard>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </RouteGuard>
+  );
 }
